4a9738ad739bbdd2d99576733a3ce8cb55571d2d
oodoabel <141529512+oodoabel@users.noreply.github.com>
2024-02-20 02:48:10 +0100

    still learning C

---
+++ Tutorial.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(){
+// DATA TYPES
+//     int age = 40;
+//     double gpa = 4.0;
+//     char grade = 'A';
+//     char phrase[] = "Hi academy";
+
+//     return 0;
+
+
+// // PRINTF
+
+printf("Hello \" world\n");
+
+int favNum = 90;
+char singChar = 'i';
+
+printf("My favourite %s is %d and its float is %f\n", "number", favNum, 500.000);
+printf("To print a single character, we use %c", singChar);
+return 0;
+
+
+
+}
+
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+int main(){
+//     int age = 40;
+//     double gpa = 4.0;
+//     char grade = 'A';
+//     char phrase[] = "Hi academy";
+
+//     return 0;
+
+
+// // PRINTF
