d982f414f892179e466e1b78451be8f14c67f790
oodoabel <141529512+oodoabel@users.noreply.github.com>
2024-02-20 06:25:04 +0100

    still learning C

--- Tutorial.c
+++ Tutorial.c
@@ -21,10 +21,40 @@ int favNum = 90;
 char singChar = 'i';
 
 printf("My favourite %s is %d and its float is %f\n", "number", favNum, 500.000);
-printf("To print a single character, we use %c", singChar);
+printf("To print a single character, we use %c\n", singChar);
+
+// WORKING WITH NUMBERS
+printf("%f\n", 5 + 4.5);
+
+printf("%f\n", pow(4, 5));
+
+printf("%f\n", sqrt(36));
+printf("%f", ceil(36.467));
+printf("%f\n\n", sqrt(36));
+
+// COMMENTS IN C
+/*
+TO DO
+print out text
+*/
+
+// CONSTANTS
+// CONATANTS ARE PIECE OF INFORMATION IN OUR CODES THAT WE DONT WANT TO MODIFY
+
+const int FAV_NUM = 5;
+printf("%d\n", FAV_NUM);
+
+// num = 8;
+
+// printf("%d", num);
+
+// GETTING INPUT FROM A USER
+
+double age;
+
+printf("Enter your age: ");
+scanf("%lf", &age);
+printf("you are %f yrs old", age);
+
 return 0;
-
-
-
 }
-
